8a31fc56dcaccdf9a486df19121e2192b738ab42
rijukumari <riju9314@gmail.com>
2025-07-09 10:56:27 +0530

    second commit

--- server/src/index.js
+++ server/src/index.js
@@ -2,33 +2,38 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/conn.js";
 import cors from "cors";
-import  cookieParser from 'cookie-parser';
-import userRouter from './routes/user.routes.js';
-import sellerRouter from './routes/seller.route.js';
-import productRouter from './routes/product.route.js'
-import cartRouter from './routes/cart.route.js';
-import orderRouter from './routes/order.route.js';
-import addressRouter from './routes/address.route.js'
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.routes.js";
+import sellerRouter from "./routes/seller.route.js";
+import productRouter from "./routes/product.route.js";
+import cartRouter from "./routes/cart.route.js";
+import orderRouter from "./routes/order.route.js";
+import addressRouter from "./routes/address.route.js";
 import { connectCloudinary } from "./db/cloudinary.js";
 
 const app = express();
 dotenv.config();
-connectCloudinary()
-const allowedOrigins = ["http://localhost:5173"];
+connectCloudinary();
 
 app.use(express.json());
-app.use(cors({
-  origin: allowedOrigins, credentials:true
-}));
 app.use(cookieParser());
 
-app.use('/images', express.static("uploads"));
-app.use('/api/user', userRouter)
-app.use('/api/seller', sellerRouter);
-app.use('/api/product', productRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
-app.use('/api/address', addressRouter);
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/seller", sellerRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/address", addressRouter);
 
 const port = process.env.PORT;
 
